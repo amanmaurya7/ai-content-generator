f5b79ccf836945dd769e18e296942a4fba80290a
Aman Maurya <amanmaurya.me@gmail.com>
2024-07-05 16:26:17 +0530

    Update FormSection.tsx

--- app/dashboard/content/_components/FormSection.tsx
+++ app/dashboard/content/_components/FormSection.tsx
@@ -1,3 +1,5 @@
+"use client";
+ 
 import React from 'react'
 import { TEMPLATE } from '../../_components/TemplateListSection'
 import Image from 'next/image'
@@ -9,16 +11,21 @@ interface PROPS{
   selectedTemplate?: TEMPLATE;
 }
 function FormSection({selectedTemplate}:PROPS) {
+
+  const onSubmit=(e:any)=>{
+    e.preventDefault();
+  }
+
   return (
     <div className='p-5 shadow-md border rounded-lg'>
       {/* @ts-ignore */}
       <Image src={selectedTemplate?.icon} alt={selectedTemplate?.name} width={70} height={70}/>
       <h2 className='font-bold text-2xl mb-2 text-primary'>{selectedTemplate?.name}</h2>
       <p className='text-gray-500 text-sm'>{selectedTemplate?.desc}</p>
-      <form>
+      <form className='mt-6' onSubmit={onSubmit}>
         {selectedTemplate?.form?.map((item,index)=>(
-            <div>
-              <label>{item.label}</label>
+            <div className='my-2 flex flex-col gap-2 mb-7'>
+              <label className='font-bold'>{item.label}</label>
               {item.field == 'input' ?
               <Input />
             : item.field == 'textarea' ?
@@ -26,7 +33,7 @@ function FormSection({selectedTemplate}:PROPS) {
               }
             </div>
         ))}
-        <Button>Generate Content</Button>
+        <Button type='submit' className='w-full py-6'>Generate Content</Button>
       </form>
     </div>
   )
