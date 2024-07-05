146e2ad5923072d03c9341b774b95eb33549d95c
Aman Maurya <amanmaurya.me@gmail.com>
2024-07-05 16:14:52 +0530

    Design the Dynamic form

--- app/dashboard/content/[template-slug]/page.tsx
+++ app/dashboard/content/[template-slug]/page.tsx
@@ -1,8 +1,27 @@
 import React from 'react'
+import FormSection from '../_components/FormSection'
+import OutputSection from '../_components/OutputSection'
+import Templates from '@/app/(data)/Templates'
+import {TEMPLATE} from '../../_components/TemplateListSection'  
 
-function CreateNewContent() {
+interface PROPS{
+  params:{
+    'template-slug':string
+  }
+}
+
+function CreateNewContent(props: PROPS) {
+
+  const selectedTemplate:TEMPLATE | undefined =Templates?.find((item)=>item.slug==props.params['template-slug'])
+  
   return (
-    <div>CreateNewContent</div>
+    <div className='grid grid-cols-1 md:grid-cols-2 gap-5 p-5'>
+      {/* Form Section */}
+      <FormSection selectedTemplate={selectedTemplate} />
+
+      {/* OutputSection */}
+      <OutputSection/>
+    </div>
   )
 }
 
