0cf8c62ad3f212929b0c76a56dfe9ea7edf2aee8
Aman Maurya <amanmaurya.me@gmail.com>
2024-07-06 15:26:43 +0530

    added gemini ai api and completed text editor

--- app/dashboard/content/[template-slug]/page.tsx
+++ app/dashboard/content/[template-slug]/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React from 'react'
+import React, {useState} from 'react'
 import FormSection from '../_components/FormSection'
 import OutputSection from '../_components/OutputSection'
 import Templates from '@/app/(data)/Templates'
@@ -7,6 +7,7 @@ import {TEMPLATE} from '../../_components/TemplateListSection'
 import { Button } from '@/components/ui/button';
 import { ArrowLeft } from 'lucide-react';
 import Link from 'next/link';
+import { chatSession } from '@/utils/AIModel';
 
 interface PROPS{
   params:{
@@ -17,9 +18,21 @@ interface PROPS{
 function CreateNewContent(props: PROPS) {
 
   const selectedTemplate:TEMPLATE | undefined =Templates?.find((item)=>item.slug==props.params['template-slug'])
+  const [loading, setLoading] = useState(false);
+  const [aiOutput, setAiOutput] = useState<string>('')
+
+  const GenerateAIContent = async(formData:any) => {
+    setLoading(true);
+
+    const SelectedPrompt = selectedTemplate?.aiPrompt;  
+
+    const FinalAIPrompt = JSON.stringify(formData) +", "+ SelectedPrompt;
   
-  const GenerateAIContent = (formData:any) => {
-  
+    const result = await chatSession.sendMessage(FinalAIPrompt);
+
+    console.log(result.response.text());
+    setAiOutput(result?.response.text());
+    setLoading(false)
   }
 
   return (
@@ -30,11 +43,13 @@ function CreateNewContent(props: PROPS) {
         <div className='grid grid-cols-1 md:grid-cols-3 gap-5 py-5'>
           {/* Form Section */}
           <FormSection selectedTemplate={selectedTemplate} 
-          userFormInput ={(v:any)=>console.log(v)}/>
+          userFormInput ={(v:any)=>GenerateAIContent(v)}
+          loading={loading}
+          />
 
           {/* OutputSection */}
           <div className='col-span-2'>
-          <OutputSection/>
+          <OutputSection aiOutput={aiOutput}/>
         </div>
         </div>
     </div>
