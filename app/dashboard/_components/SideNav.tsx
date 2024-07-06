fb7b037463ddf11ed2df9577dda9d02694031889
Aman Maurya <amanmaurya.me@gmail.com>
2024-07-06 12:10:23 +0530

    completed the dynamic form

--- app/dashboard/_components/SideNav.tsx
+++ app/dashboard/_components/SideNav.tsx
@@ -35,7 +35,7 @@ function SideNav() {
     },[])
 
   return (
-    <div className='h-screen p-5 shadow-sm border'>
+    <div className='h-screen p-5 shadow-sm border bg-white'>
         <div className='flex justify-center'>
         <Image src={'/logo.svg'} alt="logo" width={130} height={100}/>
         </div>
--- app/dashboard/content/[template-slug]/page.tsx
+++ app/dashboard/content/[template-slug]/page.tsx
@@ -1,8 +1,12 @@
+"use client";
 import React from 'react'
 import FormSection from '../_components/FormSection'
 import OutputSection from '../_components/OutputSection'
 import Templates from '@/app/(data)/Templates'
 import {TEMPLATE} from '../../_components/TemplateListSection'  
+import { Button } from '@/components/ui/button';
+import { ArrowLeft } from 'lucide-react';
+import Link from 'next/link';
 
 interface PROPS{
   params:{
@@ -14,13 +18,25 @@ function CreateNewContent(props: PROPS) {
 
   const selectedTemplate:TEMPLATE | undefined =Templates?.find((item)=>item.slug==props.params['template-slug'])
   
-  return (
-    <div className='grid grid-cols-1 md:grid-cols-2 gap-5 p-5'>
-      {/* Form Section */}
-      <FormSection selectedTemplate={selectedTemplate} />
+  const GenerateAIContent = (formData:any) => {
+  
+  }
 
-      {/* OutputSection */}
-      <OutputSection/>
+  return (
+    <div className='p-10'>
+      <Link href={"/dashboard"}>
+      <Button> <ArrowLeft/> Back </Button>
+      </Link>
+        <div className='grid grid-cols-1 md:grid-cols-3 gap-5 py-5'>
+          {/* Form Section */}
+          <FormSection selectedTemplate={selectedTemplate} 
+          userFormInput ={(v:any)=>console.log(v)}/>
+
+          {/* OutputSection */}
+          <div className='col-span-2'>
+          <OutputSection/>
+        </div>
+        </div>
     </div>
   )
 }
